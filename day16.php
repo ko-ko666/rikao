b00a32ba4192168d1fba4300e15f0e440c7df309
ko-ko666 <1034211486@qq.com>
2019-04-28 08:57:54 +0800

    日考技能

---
+++ day16.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+$a = array([1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]);
+// var_dump($a);
+$target = 7;
+
+$b = 7;
+// var_dump($b);exit;
+
+
+if($b == 7){
+	echo "成功";
+	return true;
+}
+
+ ?>
