e9684f43f446e9aaed4e6ec1f828e3bb9991c20f
ko-ko666 <1034211486@qq.com>
2019-05-14 08:56:46 +0800

    日考资料

---
+++ day4.php
@@ -0,0 +1,23 @@
+<?php 
+
+class calSteps($n){
+	for ($i=0; $i <=$n ; $i++) { 
+		$i+($i+1)+=$n;
+		break;
+	}
+	for ($j=0; $j <=$n ; $j++) { 
+		$i+=$n;
+		break;
+	}
+	for ($x=0; $x <=$n; $x++) { 
+		($i+1)+=$n;
+		break;
+	}
+	return $n;
+}
+
+
+
+
+
+ ?>
